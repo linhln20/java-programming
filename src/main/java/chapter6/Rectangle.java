e67b57c6f8210382f7cebd0d169439c6ab72696a
Linh Lam <ngoclinhlam95@gmail.com>
2025-02-27 11:22:18 +0700

    update chapter2-add SeasonOfYear.java; add chapter6

--- src/main/java/chapter6/Rectangle.java
+++ src/main/java/chapter6/Rectangle.java
@@ -1,5 +1,41 @@
 package chapter6;
 
 public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    public Rectangle(double length, double width){
+        this.length = length; //option 1
+        setWidth(width); // or option 2
+    }
+
+    public double getLength() {
+        return length;
+    }
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double calculatePerimeter(){
+        return (length+width)*2;
+    }
+
+    public double calculateArea() {
+        return length*width;
+    }
+
 
 }
